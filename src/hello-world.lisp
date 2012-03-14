ec95d326c5272b59e6669551e8ebc7ff271a9376
Jose Santos <jsmpereira@gmail.com>
2012-03-14 12:51:49 +0000

    Testing aserve.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,24 +1,24 @@
-(in-package :hunchentoot)
+;(in-package :hunchentoot)
+(in-package :net.aserve)
 
-(hunchentoot:define-easy-handler (hello-sbcl :uri "/hello-sbcl") (name)
-  (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Hey~@[ ~A~]! ~A ~A, Hunchentoot ~A" name (lisp-implementation-type) (lisp-implementation-version) hunchentoot::*hunchentoot-version*))
+;(hunchentoot:define-easy-handler (hello-sbcl :uri "/hello-sbcl") (name)
+;  (setf (hunchentoot:content-type*) "text/plain")
+;  (format nil "Hey~@[ ~A~]! ~A ~A, Hunchentoot ~A" name (lisp-implementation-type) (lisp-implementation-version) hunchentoot::*hunchentoot-version*))
 
-;(publish :path "/"
-;	 :function #'(lambda (req ent)
-;		       (with-http-response (req ent)
-;			 (with-http-body (req ent)
-;			   (html
-;			    (:h1 "Hello World")
-;			    (:princ "Congratulations, you are running Lisp on Heroku!!!")
-;			    :p
-;			    ((:img :src "lisp-glossy.jpg"))
-;			    )))))
-;
-;(publish-directory
-; :prefix "/"
-; :destination "./public/"
-; )
+(publish :path "/" :function #'(lambda (req ent)
+		       (with-http-response (req ent)
+			 (with-http-body (req ent)
+			   (html
+			    (:h1 "Hello World")
+			    (:princ "Congratulations, you are running Lisp on Heroku!!!")
+			    :p
+			    ((:img :src "lisp-glossy.jpg"))
+			    )))))
+
+(publish-directory
+ :prefix "/"
+ :destination "./public/"
+ )
 
 		   
 
