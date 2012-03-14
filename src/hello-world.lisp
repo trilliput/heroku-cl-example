973df077b2ea521b6f41f13fd53ae2005250fdc5
Jose Santos <jsmpereira@gmail.com>
2012-03-14 16:25:32 +0000

    Cleanup hunchentoot related code.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,9 +1,4 @@
-;(in-package :hunchentoot)
 (in-package :net.aserve)
-
-;(hunchentoot:define-easy-handler (hello-sbcl :uri "/hello-sbcl") (name)
-;  (setf (hunchentoot:content-type*) "text/plain")
-;  (format nil "Hey~@[ ~A~]! ~A ~A, Hunchentoot ~A" name (lisp-implementation-type) (lisp-implementation-version) hunchentoot::*hunchentoot-version*))
 
 (publish :path "/" :function #'(lambda (req ent)
 		       (with-http-response (req ent)
