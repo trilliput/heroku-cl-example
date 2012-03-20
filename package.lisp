948b8545c793b5367a9ffdb8be9d153ecbc13f08
Jose Santos <jsmpereira@gmail.com>
2012-03-20 16:47:01 +0000

    Fix html output.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,5 @@
 ;;;; package.lisp
 
 (defpackage #:example
-  (:use #:cl))
+  (:use #:cl #:net.aserve)
+  (:import-from #:net.aserve *html-stream*))
--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,24 +1,25 @@
 (in-package :example)
 
-(net.aserve:publish :path "/" :function #'(lambda (req ent)
-				 (with-http-response (request entity :content-type "text/html")
-				   (with-http-body (request entity)
-				     (cl-who:with-html-output-to-string (s)
-				       (:html
-					 (:head
-					  (:title "Heroku CL Example App"))
-					 (:body
-					  (:h1 "Heroku CL Example App")
-					  (:h3 "Using")
-					  (:ul
-					   (:li (format s "~A ~A" (lisp-implementation-type) (lisp-implementation-version)))
-					   (:li (format s "~A" net.aserve:*aserve-version*))
-					   (:li (format s "CL-WHO")))
-					  (:div
-					   (:a :href "static/lisp-glossy.jpg" (:img :src "static/lisp-glossy.jpg" :width 100)))
-					  (:div
-					   (:a :href "static/hello.txt" "hello")))))))))
+(publish :path "/"
+	 	:function #'(lambda (req ent)
+	 					(with-http-response (req ent)
+	 						(with-http-body (req ent)
+			 				  	(cl-who:with-html-output (s *html-stream*)
+									(:html
+									 (:head
+									  (:title "Heroku CL Example App"))
+									 (:body
+									  (:h1 "Heroku CL Example App")
+									  (:h3 "Using")
+									  (:ul
+									   (:li (format s "~A ~A" (lisp-implementation-type) (lisp-implementation-version)))
+									   (:li (format s "Portable AllegroServe ~{~A~^.~}" net.aserve:*aserve-version*))
+									   (:li (format s "CL-WHO")))
+									  (:div
+									   (:a :href "static/lisp-glossy.jpg" (:img :src "static/lisp-glossy.jpg" :width 100)))
+									  (:div
+									   (:a :href "static/hello.txt" "hello")))))))))
 
-(net.aserve:publish-directory
- :prefix "/"
+(publish-directory
+ :prefix "/static/"
  :destination "./public/")
