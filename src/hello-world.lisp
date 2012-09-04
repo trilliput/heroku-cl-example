523feb30afc5f1026b6965f79b1e16708fb01ebe
Jose Santos <jsmpereira@gmail.com>
2012-09-04 04:18:22 +0100

    Fix DATABASE_URL parsing. Recent changes to the Heroku db infrastucture resulted in the addition of the port to the db config var. This caused POSTMODERN to receive malformed params. Thanks to @bhoung for the initial report.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -9,12 +9,12 @@
 (defvar *database-url* (heroku-getenv "DATABASE_URL"))
 
 (defun db-params ()
-  "Heroku database url format is postgres://username:password@host/database_name.
+  "Heroku database url format is postgres://username:password@host:port/database_name.
 TODO: cleanup code."
   (let* ((url (second (cl-ppcre:split "//" *database-url*)))
 	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
-	 (host (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))
+	 (host (first (cl-ppcre:split ":" (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))))
 	 (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
     (list database user password host)))
 
