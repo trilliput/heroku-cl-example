ace654d666ea198b3396eb331643df7a1021829e
Jose Santos <jsmpereira@gmail.com>
2012-09-05 01:05:01 +0100

    Fix weird dynamic variable behavior. Use lexical scoping.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -5,13 +5,10 @@
   #+ccl (ccl:getenv target)
   #+sbcl (sb-posix:getenv target))
 
-;; Database
-(defvar *database-url* (heroku-getenv "DATABASE_URL"))
-
 (defun db-params ()
   "Heroku database url format is postgres://username:password@host:port/database_name.
 TODO: cleanup code."
-  (let* ((url (second (cl-ppcre:split "//" *database-url*)))
+  (let* ((url (second (cl-ppcre:split "//" (heroku-getenv "DATABASE_URL"))))
 	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (host (first (cl-ppcre:split ":" (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))))
