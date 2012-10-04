ffde82894b58ceb6dac52cbd66aa593ab2923571
Trilliput <root@trilliput.ru>
2012-10-04 23:58:57 +0700

    #128 css improovments related to footer

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -17,18 +17,19 @@
       (:a :href "https://github.com/Trilliput" 
        (:img :class "forkme" :src "https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" :alt "Fork me on GitHub"))
       (:div :class "container"
-       (:div :class "tweets")
-       (:div :class "content"
-        (:h2 "Common Lisp on Heroku")
+       (:div :class "sub-container"
+        (:div :class "tweets")
+        (:div :class "content"
+         (:h2 "Common Lisp on Heroku")
 
-        (:p :class "h-notes" "Sandbox Project")
+         (:p :class "h-notes" "Sandbox Project")
 
-        (:p "This is my sandbox project to play around with Common Lisp on the Heroku cloud using " (:a :href "https://github.com/jsmpereira/heroku-buildpack-cl" "a fork by José Santos Martins Pereira") "of the " (:a :href "https://github.com/mtravers/heroku-buildpack-cl" "Heroku Common Lisp Buildpack originally developed by Mike Travers") ". See the buildpack repository for more information and credits.")
+         (:p "This is my sandbox project to play around with Common Lisp on the Heroku cloud using " (:a :href "https://github.com/jsmpereira/heroku-buildpack-cl" "a fork by José Santos Martins Pereira") "of the " (:a :href "https://github.com/mtravers/heroku-buildpack-cl" "Heroku Common Lisp Buildpack originally developed by Mike Travers") ". See the buildpack repository for more information and credits.")
 
-        (:p "This is the Hunchentoot branch. I'm focused only on Hunchentoot now, so other branch(es) was forked from the examle application originally made by José Santos Martins Pereira")
+         (:p "This is the Hunchentoot branch. I'm focused only on Hunchentoot now, so other branch(es) was forked from the examle application originally made by José Santos Martins Pereira")
 
-        (:p "Url to my sandbox app: " (:a :href "http://sleepy-peak-6043.herokuapp.com/" "http://sleepy-peak-6043.herokuapp.com/")))
-        (:div :class "container-float-cleaner"))
+         (:p "Url to my sandbox app: " (:a :href "http://sleepy-peak-6043.herokuapp.com/" "http://sleepy-peak-6043.herokuapp.com/")))
+         (:div :class "container-float-cleaner")))
 
       (:div :class "footer"
        (:div :class "powered-by"
@@ -39,4 +40,5 @@
          (:li (format s "~A ~A" (lisp-implementation-type) (lisp-implementation-version)))
          (:li (format s "Hunchentoot ~A" hunchentoot::*hunchentoot-version*))
          (:li (format s "CL-WHO"))
-         (:li (format s "~A" (caar (postmodern:with-connection (db-params) (postmodern:query "select version()"))))))))))))
+         (:li (format s "~A" (caar (postmodern:with-connection (db-params) (postmodern:query "select version()"))))))
+       (:div :class "footer-float-cleaner")))))))
