21d31ee2e70c1534fe50907f1308152746d639ab
Trilliput <root@trilliput.ru>
2012-10-04 23:03:00 +0700

    #128 Added text of README from github

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -12,13 +12,22 @@
       (:script :language "javascript" :src "static/jquery.tweet.js" :type "text/javascript")
       (:script :language "javascript" :src "static/main.js" :type "text/javascript")
       (:link :rel "stylesheet" :type "text/css" :href "static/main.css")
-      (:title "Heroku CL Example App"))
+      (:title "Heroku CL Hello World"))
      (:body
       (:a :href "https://github.com/Trilliput" 
        (:img :class "forkme" :src "https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" :alt "Fork me on GitHub"))
       (:div :class "tweets")
       (:div :class "content"
-      (:h1 "Heroku CL Example App")
+      (:h2 "Common Lisp on Heroku")
+
+      (:p :class "h-notes" "Sandbox Project")
+
+      (:p "This is my sandbox project to play around with Common Lisp on the Heroku cloud using " (:a :href "https://github.com/jsmpereira/heroku-buildpack-cl" "a fork by José Santos Martins Pereira") "of the " (:a :href "https://github.com/mtravers/heroku-buildpack-cl" "Heroku Common Lisp Buildpack originally developed by Mike Travers") ". See the buildpack repository for more information and credits.")
+
+      (:p "This is the Hunchentoot branch. I'm focused only on Hunchentoot now, so other branch(es) was forked from the examle application originally made by José Santos Martins Pereira")
+
+      (:p "Url to my sandbox app: " (:a :href "http://sleepy-peak-6043.herokuapp.com/" "http://sleepy-peak-6043.herokuapp.com/"))
+
       (:h3 "Using")
       (:ul
        (:li (format s "~A ~A" (lisp-implementation-type) (lisp-implementation-version)))
