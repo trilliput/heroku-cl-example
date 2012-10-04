ff3fc7eca2663510c23736c0b130daffc1a24921
Trilliput <root@trilliput.ru>
2012-10-04 17:42:02 +0700

    Improvements in local setup script

--- local-setup.lisp
+++ local-setup.lisp
@@ -1,4 +1,7 @@
 (in-package :cl-user)
 
 (ql:quickload :example)  ; (asdf:load-system :example)
-(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242))
+
+(defvar *hunchentoot-instance* (make-instance 'hunchentoot:easy-acceptor :port 4242))
+(hunchentoot:start *hunchentoot-instance*)
+;(hunchentoot:stop *hunchentoot-instance*)
