9abbb10dcd9900c853811c7b293b77ff62f81b4e
Trilliput <root@trilliput.ru>
2012-10-08 23:47:05 +0700

    #135 made loading config file process to be verbose

--- core/init.lisp
+++ core/init.lisp
@@ -7,8 +7,8 @@
 (defun init-configs () 
   (and 
     (in-package :core)
-    (load "settings/settings.lisp")
-    (if (probe-file "settings/local-settings.lisp") (load "settings/local-settings.lisp"))))
+    (load "settings/settings.lisp" :verbose t)
+    (if (probe-file "settings/local-settings.lisp") (load "settings/local-settings.lisp" :verbose t))))
 
 (defun get-db-params ()
   (utils-heroku-or-local-db-params))
