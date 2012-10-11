e82eb54505cdf046dd8efe19c18642a6d1d18dfe
Trilliput <root@trilliput.ru>
2012-10-11 09:38:08 +0700

    #139 Added closing current toplevel connection to core:init-db function and replaced all AND with PROGN in core/init script

--- core/init.lisp
+++ core/init.lisp
@@ -1,18 +1,20 @@
 (in-package :core)
 
 (defun init ()
-  (and
+  (progn
     (init-configs)
     (init-db)))
 
 (defun init-configs () 
-  (and 
+  (progn 
     (in-package :core)
     (if (probe-file "settings/local-settings.lisp") (load "settings/settings.lisp" :verbose t)) ;added file existence check espcially to make Heroku work. TODO: Had to be solved later
     (if (probe-file "settings/local-settings.lisp") (load "settings/local-settings.lisp" :verbose t))))
 
 (defun init-db ()
-  (apply #'postmodern:connect-toplevel (get-db-params)))
+  (progn 
+    (if (not (null postmodern:*database*)) (postmodern:disconnect-toplevel))
+    (apply #'postmodern:connect-toplevel (get-db-params))))
 
 (defun get-db-params ()
   (utils-heroku-or-local-db-params))
